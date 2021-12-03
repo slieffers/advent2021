2b6dcfb4ab0f582f1c59c35114ebdbeb7fd0d64f
Shane Lieffers <TuthmosisIII@msn.com>
2021-12-02 21:06:23 -0500

    Cleanup

--- Day2/Day2.fs
+++ Day2/Day2.fs
@@ -4,14 +4,14 @@ module Day2 =
     let extractCoordValue (coord: string) =
             coord.Split(' ').[1] |> int32
             
-    let calculatePosition (horiz: int32, vert: int32) (coord: string) =
+    let calculatePosition (horiz, vert) (coord: string) =
         match coord.Split(' ').[0] with
             | "forward" -> (horiz + extractCoordValue coord, vert)
             | "up" -> (horiz, vert - extractCoordValue coord)
             | "down" -> (horiz, vert + extractCoordValue coord)
             | _ -> (horiz, vert)
         
-    let calculatePositionWithAim (horiz: int32, vert: int32, aim: int32) (coord: string) =
+    let calculatePositionWithAim (horiz, vert, aim) (coord: string) =
         match coord.Split(' ').[0] with
             | "forward" -> (horiz + extractCoordValue coord, vert + aim * extractCoordValue coord, aim)
             | "up" -> (horiz, vert, aim - extractCoordValue coord)

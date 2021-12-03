83f90bdbdda46c7fe95a5c71ca4e27f3571e8ce2
Shane Lieffers <TuthmosisIII@msn.com>
2021-12-02 21:06:23 -0500

    Cleanup

--- Day2/Day2.fs
+++ Day2/Day2.fs
@@ -4,24 +4,24 @@ module Day2 =
     let extractCoordValue (coord: string) =
             coord.Split(' ').[1] |> int32
             
-    let calculatePosition (horiz: int32, vert: int32) (coord: string) =
+    let calculatePosition (horiz, vert) (coord: string) =
         match coord.Split(' ').[0] with
             | "forward" -> (horiz + extractCoordValue coord, vert)
             | "up" -> (horiz, vert - extractCoordValue coord)
             | "down" -> (horiz, vert + extractCoordValue coord)
             | _ -> (horiz, vert)
         
-    let calculatePositionWithAim (horiz: int32, vert: int32, aim: int32) (coord: string) =
+    let calculatePositionWithAim (horiz, vert, aim) (coord: string) =
         match coord.Split(' ').[0] with
             | "forward" -> (horiz + extractCoordValue coord, vert + aim * extractCoordValue coord, aim)
             | "up" -> (horiz, vert, aim - extractCoordValue coord)
             | "down" -> (horiz, vert, aim + extractCoordValue coord)
             | _ -> (horiz, vert, aim)
         
-    let findFinalPositionValue (coordinates: seq<string>) =
+    let findFinalPositionValue coordinates =
         Seq.fold calculatePosition (0, 0) coordinates
         |> (fun (a, b) -> a * b)
         
-    let findFinalPositionValueWithAim (coordinates: seq<string>) =
+    let findFinalPositionValueWithAim coordinates =
         Seq.fold calculatePositionWithAim (0, 0, 0) coordinates
         |> (fun (a, b, _) -> a * b)
